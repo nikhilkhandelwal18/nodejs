2d725f42328035e692fcd7ef2b9eebfcf6c2e87c
nikhilkhandelwal18 <nikhil.khandelwal@nitorinfotech.net>
2017-08-29 10:54:47 +0530

    express routing

--- web-server/app-express.js
+++ web-server/app-express.js
@@ -26,4 +26,17 @@ app.get('/api', function(req, res) {
 
 })
 
+app.get('/person/:id', function(req, res) {
+    //OR any pattern matching
+//app.get('/person/:page/:id', function(req, res) {
+    res.send(`<html>
+    <head></head>
+    <body> ${req.params.id}
+        <h1> Person: ` + req.params.id + `</h1>
+    </body>
+    </html>`);
+
+});
+
+
 app.listen(port);
